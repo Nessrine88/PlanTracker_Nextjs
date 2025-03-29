f62cd0367331ac8671355ee0deae6f9efae488f8
Nessrine88 <macherki.nessrine@yahoo.com>
2025-03-29 19:02:21 +0100

    fix errors

--- app/components/logout.tsx
+++ app/components/logout.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import ProtectedRoute from '../components/ProtectedRoute';
 
 const Logout = () => {
   const [loading, setLoading] = useState(false);
